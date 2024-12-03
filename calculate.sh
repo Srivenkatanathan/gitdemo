1b994f12418644444320e39b782babf3de885628
Ubuntu <ubuntu@ip-172-31-7-49.ap-south-1.compute.internal>
2024-12-03 17:47:15 +0000

    This the First Version of current application.

---
+++ calculate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+####################################################
+
+#Author : Srivenkatanathan S
+
+#Date : 03-12-2024
+
+#The purspose of this script is to subract 2 numbers
+
+####################################################
+
+
+a=8-2
+
+echo $a
+
+
