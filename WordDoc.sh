a6564b3493b9eeeb6c1f7f42cfaaf0dc4c35d3ca
Ubuntu <ubuntu@ip-172-31-7-49.ap-south-1.compute.internal>
2024-12-04 15:00:00 +0000

    The first Version of WordDoc

---
+++ WordDoc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#######################################################
+
+#Author: Srivenkatanathan S
+
+#Date: 02-12-2024
+
+#The purpose of this file is to practice Git commands
+
+######################################################
+
+
+
+echo "I am writing the Git Commands below here!!"
+
+echo "The 3 important Git command everyone needs to know is 1.git add 2.git commit and 3.git push"
+
